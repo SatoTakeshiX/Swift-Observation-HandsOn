ce8e9a6d73ecb82c64c1e581ba24a30db8381680
satoutakeshi <sato.takeshi1987@gmail.com>
2023-11-03 01:00:13 +0900

    add test

---
+++ Chapter1/Chapter1Tests/Chapter1Tests.swift
@@ -0,0 +1,55 @@
+//
+//  Chapter1Tests.swift
+//  Chapter1Tests
+//
+//  Created by satoutakeshi on 2023/11/02.
+//
+
+import XCTest
+@testable import Chapter1
+
+final class Chapter1Tests: XCTestCase {
+    func testTrackingStoredProperty() {
+        let book = BookAccount()
+        let exp = XCTestExpectation()
+
+        withObservationTracking {
+            print(book.isBorrowed)
+        } onChange: {
+            exp.fulfill()
+        }
+
+        book.isBorrowed = true
+        wait(for: [exp], timeout: 0.5)
+    }
+
+    func testTrackingCompuredProperty() {
+        let book = BookAccount()
+        let exp = XCTestExpectation()
+
+        withObservationTracking {
+            print(book.borrowedCount)
+        } onChange: {
+            exp.fulfill()
+        }
+
+        book.history[Date()] = true
+        wait(for: [exp], timeout: 0.5)
+    }
+
+    func testNotTracking() {
+        let book = BookAccount()
+        let exp = XCTestExpectation()
+        exp.isInverted = true
+
+        withObservationTracking {
+            print(book.isBorrowed)
+        } onChange: {
+            // no one call this
+            exp.fulfill()
+        }
+
+        book.history[Date()] = false
+        wait(for: [exp], timeout: 0.5)
+    }
+}
