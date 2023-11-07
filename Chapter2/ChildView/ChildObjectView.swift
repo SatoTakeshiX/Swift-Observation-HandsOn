ae1eb333c2bcdbd8782a98568b805dc8813abd39
satoutakeshi <sato.takeshi1987@gmail.com>
2023-11-07 22:57:56 +0900

    add ChildObjectView

---
+++ Chapter2/ChildView/ChildObjectView.swift
@@ -0,0 +1,18 @@
+//
+//  ChildObjectView.swift
+//  Chapter2
+//
+//  Created by satoutakeshi on 2023/11/07.
+//
+
+import SwiftUI
+
+struct ChildObjectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChildObjectView()
+}
