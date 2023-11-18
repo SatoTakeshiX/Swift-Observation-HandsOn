6cdf6bdd846afe57d2fe99d710b3f62fa52c8d92
satoutakeshi <sato.takeshi1987@gmail.com>
2023-11-19 01:36:35 +0900

    add chapter 4 code

---
+++ Chapter4/Chapter4/Chapter4App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter4App.swift
+//  Chapter4
+//
+//  Created by satoutakeshi on 2023/11/18.
+//
+
+import SwiftUI
+
+@main
+struct Chapter4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chapter4/Chapter4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chapter4
+//
+//  Created by satoutakeshi on 2023/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
