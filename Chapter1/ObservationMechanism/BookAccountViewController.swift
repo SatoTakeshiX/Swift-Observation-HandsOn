0249b1fa9591a73cdbcddad58c60cc807a19d013
satoutakeshi <sato.takeshi1987@gmail.com>
2023-11-27 00:13:59 +0900

    Lは大文字

--- Chapter1/ObservationMechanism/BookAccountViewController.swift
+++ Chapter1/ObservationMechanism/BookAccountViewController.swift
@@ -21,14 +21,14 @@ struct BookAccountView: UIViewControllerRepresentable {
 final class BookAccountViewController: UIViewController {
     private let bookAccount = BookAccount()
     private var flg: Bool = false
-    private var borrowedStatelabel: UILabel = .init()
-    private var borrowedCountlabel: UILabel = .init()
+    private var borrowedStateLabel: UILabel = .init()
+    private var borrowedCountLabel: UILabel = .init()
     private var button: UIButton = .init()
     private let imageView = UIImageView(image: UIImage(systemName: "book"))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        borrowedStatelabel.text = "貸出可能"
+        borrowedStateLabel.text = "貸出可能"
         button.setTitle("この本を借りる", for: .normal)
         button.addTarget(self, action: #selector(changeBorrowed), for: .touchUpInside)
 
@@ -40,14 +40,14 @@ final class BookAccountViewController: UIViewController {
         withObservationTracking { [weak self] in
             guard let self else { return }
             if self.bookAccount.isBorrowed {
-                self.borrowedStatelabel.text = "貸出中"
+                self.borrowedStateLabel.text = "貸出中"
                 self.button.setTitle("この本を返す", for: .normal)
             } else {
-                self.borrowedStatelabel.text = "貸出可能"
+                self.borrowedStateLabel.text = "貸出可能"
                 self.button.setTitle("この本を借りる", for: .normal)
             }
 
-            self.borrowedCountlabel.text = "貸出回数: \(self.bookAccount.borrowedCount)"
+            self.borrowedCountLabel.text = "貸出回数: \(self.bookAccount.borrowedCount)"
         } onChange: { [weak self] in
             guard let self else { return }
             Task { @MainActor in
@@ -76,26 +76,26 @@ final class BookAccountViewController: UIViewController {
 
     func setupUI() {
         view.addSubview(imageView)
-        view.addSubview(borrowedStatelabel)
-        view.addSubview(borrowedCountlabel)
+        view.addSubview(borrowedStateLabel)
+        view.addSubview(borrowedCountLabel)
         view.addSubview(button)
 
         imageView.contentMode = .scaleAspectFill
 
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        borrowedStatelabel.translatesAutoresizingMaskIntoConstraints = false
-        borrowedCountlabel.translatesAutoresizingMaskIntoConstraints = false
+        borrowedStateLabel.translatesAutoresizingMaskIntoConstraints = false
+        borrowedCountLabel.translatesAutoresizingMaskIntoConstraints = false
         button.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
             imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
             imageView.heightAnchor.constraint(equalToConstant: 88),
             imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            borrowedStatelabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
-            borrowedStatelabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            borrowedCountlabel.topAnchor.constraint(equalTo: borrowedStatelabel.bottomAnchor, constant: 8),
-            borrowedCountlabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.topAnchor.constraint(equalTo: borrowedCountlabel.bottomAnchor, constant: 24),
+            borrowedStateLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            borrowedStateLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            borrowedCountLabel.topAnchor.constraint(equalTo: borrowedStateLabel.bottomAnchor, constant: 8),
+            borrowedCountLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: borrowedCountLabel.bottomAnchor, constant: 24),
             button.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
 
